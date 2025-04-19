b3061922e0e625401124b8cf0d3962bebad6ed19
k23-d <pabkarna@gmail.com>
2025-04-19 00:57:34 -0400

    Update run.py

--- server/run.py
+++ server/run.py
@@ -1,6 +1,9 @@
 import uvicorn
 import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))  # Ensure current folder in path
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
