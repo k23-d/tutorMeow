60d9333e5a95b4dc3b0b8341af95a021fc8fff24
k23-d <pabkarna@gmail.com>
2025-04-20 00:07:56 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import FileUpload from '@/components/UploadSection.vue'
 
 
 export default defineConfig({
